3dc1c661fa6162cd3b7ff9646232e256b039f808
mircea <mirceashark2007@yahoo.com>
2020-04-25 20:17:41 +0300

    Fixing web api

--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,7 @@ namespace MyApi
         {
             services.AddDbContext<DataContext>( x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +40,8 @@ namespace MyApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
